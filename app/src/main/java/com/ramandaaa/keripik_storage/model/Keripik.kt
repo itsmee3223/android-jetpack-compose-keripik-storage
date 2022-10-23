3dc9a0f70936e32a406cb15ec4b52157047c4166
itsmee3223 <tamvam.pemberani@gmail.com>
2022-10-23 13:40:45 +0700

    add kerpik model

---
+++ app/src/main/java/com/ramandaaa/keripik_storage/model/Keripik.kt
@@ -0,0 +1,9 @@
+package com.ramandaaa.keripik_storage.model
+
+import android.os.Parcelable
+
+class Keripik (
+    var namaKeripik: String?="",
+    var stockKeripik: String?="",
+    var gambarKeripik: String?=""
+)
